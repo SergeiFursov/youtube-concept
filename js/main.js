8d2b4d1689ccdc6f15b01874a64b9011defcb9df
Сергей Фурсов <50398195+SergeiFursov@users.noreply.github.com>
2021-04-09 14:05:20 +0500

    Сделал адаптивность, добавил кнопку подписки.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 const swiper = new Swiper('.channel-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+      slidesPerView: 5
+    },
+     1300: {
+      slidesPerView: 4
+    },
+     1100: {
+      slidesPerView: 3
+    },
+     700: {
+      slidesPerView: 2
+    },
+  },
 
   // Navigation arrows
   navigation: {
@@ -13,7 +31,17 @@ const swiper = new Swiper('.channel-slider', {
 const swiperTwo = new Swiper('.channel-slider-two', {
   // Optional parameters
   loop: true,
-  slidesPerView: 3,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1600: {
+      slidesPerView: 3
+    },
+     1100: {
+      slidesPerView: 2
+    },
+  
+  },
 
   // Navigation arrows
   navigation: {
@@ -25,11 +53,41 @@ const swiperTwo = new Swiper('.channel-slider-two', {
 const swiperThree = new Swiper('.channel-slider-three', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
-
+  slidesPerView: 1,
+  spaceBetween: 20,
+ breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+      slidesPerView: 5
+    },
+     1300: {
+      slidesPerView: 4
+    },
+     1100: {
+      slidesPerView: 3
+    },
+     700: {
+      slidesPerView: 2
+    },
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.channel-button-right',
     prevEl: '.channel-button-left',
   }, 
 });
+
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open');
+})
+
+if (document.documentElement.scrollWidth <= 640) {
+  swiper.destroy();
+  swiperTwo.destroy();
+  swiperThree.destroy();
+}
